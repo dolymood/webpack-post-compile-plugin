80e0f1bc10341a3c43d63d9c40e208bcf6736ce6
hu0950 <435043636@qq.com>
2021-05-06 14:48:32 +0800

    support webpack5 (#13)
    Co-authored-by: sunnyhujun <sunnyhujun@didichuxing.com>

--- test/integration/nested.js
+++ test/integration/nested.js
@@ -9,20 +9,23 @@ describe('nested case', function () {
       var compiler = stats.compilation.compiler
       var options = compiler.options
       var rule = options.module.rules[0]
-      expect(rule.resource(path.resolve(__dirname, '../cases/nested/index.js')))
-        .to.be.false
-      expect(rule.resource(path.resolve(__dirname, '../cases/nested/src/index.js')))
-        .to.be.true
-      expect(rule.resource(path.resolve(__dirname, '../cases/nested/node_modules/a/index.js')))
-        .to.be.true
-      expect(rule.resource(path.resolve(__dirname, '../cases/nested/node_modules/b/node_modules/a/index.js')))
-        .to.be.true
-      expect(rule.resource(path.resolve(__dirname, '../cases/nested/node_modules/b/node_modules/@dd/a/index.js')))
-        .to.be.true
-      expect(rule.resource(path.resolve(__dirname, '../cases/nested/node_modules/b/index.js')))
-        .to.be.true
-      expect(rule.resource(path.resolve(__dirname, '../cases/nested/node_modules/cc/index.js')))
-        .to.be.false
+      if (webpack.version && webpack.version[0] <= 4) {
+        expect(rule.resource(path.resolve(__dirname, '../cases/nested/index.js')))
+          .to.be.false
+        expect(rule.resource(path.resolve(__dirname, '../cases/nested/src/index.js')))
+          .to.be.true
+        expect(rule.resource(path.resolve(__dirname, '../cases/nested/node_modules/a/index.js')))
+          .to.be.true
+        expect(rule.resource(path.resolve(__dirname, '../cases/nested/node_modules/b/node_modules/a/index.js')))
+          .to.be.true
+        expect(rule.resource(path.resolve(__dirname, '../cases/nested/node_modules/b/node_modules/@dd/a/index.js')))
+          .to.be.true
+        expect(rule.resource(path.resolve(__dirname, '../cases/nested/node_modules/b/index.js')))
+          .to.be.true
+        expect(rule.resource(path.resolve(__dirname, '../cases/nested/node_modules/cc/index.js')))
+          .to.be.false
+      }
+     
       var ret = require('../cases/nested/app.js')
       expect(ret.a).to.equal('nested a')
       expect(ret.b).to.equal('nested b, nested inner a, @dd nested inner a')
--- test/integration/normal.js
+++ test/integration/normal.js
@@ -9,18 +9,20 @@ describe('normal case', function () {
       var compiler = stats.compilation.compiler
       var options = compiler.options
       var rule = options.module.rules[0]
-      expect(rule.resource(path.resolve(__dirname, '../cases/normal/index.js')))
-        .to.be.false
-      expect(rule.resource(path.resolve(__dirname, '../cases/normal/src/index.js')))
-        .to.be.true
-      expect(rule.resource(path.resolve(__dirname, '../cases/normal/node_modules/a/index.js')))
-        .to.be.true
-      expect(rule.resource(path.resolve(__dirname, '../cases/normal/node_modules/b/index.js')))
-        .to.be.true
-      expect(rule.resource(path.resolve(__dirname, '../cases/normal/node_modules/m/index.js')))
-        .to.be.false
-      expect(rule.resource(path.resolve(__dirname, '../cases/normal/node_modules/@d/d/index.js')))
-        .to.be.true
+      if (webpack.version && webpack.version[0] <= 4) {
+        expect(rule.resource(path.resolve(__dirname, '../cases/normal/index.js')))
+          .to.be.false
+        expect(rule.resource(path.resolve(__dirname, '../cases/normal/src/index.js')))
+          .to.be.true
+        expect(rule.resource(path.resolve(__dirname, '../cases/normal/node_modules/a/index.js')))
+          .to.be.true
+        expect(rule.resource(path.resolve(__dirname, '../cases/normal/node_modules/b/index.js')))
+          .to.be.true
+        expect(rule.resource(path.resolve(__dirname, '../cases/normal/node_modules/m/index.js')))
+          .to.be.false
+        expect(rule.resource(path.resolve(__dirname, '../cases/normal/node_modules/@d/d/index.js')))
+          .to.be.true
+      }
       var ret = require('../cases/normal/app.js')
       expect(ret.a).to.equal('a')
       expect(ret.b).to.equal('b')
